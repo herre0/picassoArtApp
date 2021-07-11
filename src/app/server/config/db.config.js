054408bbff6613148ce9ca8e89bbc5ba7f588d5d
Mert Kabadayı <mertkabadayii@hotmail.com>
2021-07-11 16:15:48 +0300

    screenshots are added

--- src/app/server/config/db.config.js
+++ src/app/server/config/db.config.js
@@ -1,14 +1,27 @@
- module.exports = {
-     HOST: "localhost",
-     USER: "root",
-     PASSWORD: "xxx",
-     DB: "picassodb",
-     dialect: "mysql",
-     pool: {
-       max: 5,
-       min: 0,
-       acquire: 30000,
-       idle: 10000
-     }
-   };
+// module.exports = {
+//     HOST: "localhost",
+//     USER: "root",
+//     PASSWORD: "xperiamiro1",
+//     DB: "picassodb",
+//     dialect: "mysql",
+//     pool: {
+//       max: 5,
+//       min: 0,
+//       acquire: 30000,
+//       idle: 10000
+//     }
+//   };
 
+  module.exports = { // heroku modes
+    HOST: "pei17y9c5bpuh987.chr7pe7iynqr.eu-west-1.rds.amazonaws.com",
+    USER: "tkszslu1cmsorjm9",
+    PASSWORD: "aho627lpca7l56va",
+    DB: "fi4ylbhsjhahp7we",
+    dialect: "mysql",
+    pool: {
+      max: 5,
+      min: 0,
+      acquire: 30000,
+      idle: 10000
+    }
+  };
